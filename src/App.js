15afff3dd3a691398903909c21a138514f077672
Адель <selly.lady.zakirova@gmail.com>
2021-09-09 17:55:23 +0500

    homework2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import Layout from './components/layout/layout';
+import Footer from './components/footer/footer';
+import background1 from './assets/bg1.jpeg';
+import background2 from './assets/bg2.jpeg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world, React.JS!
-        </p>
-      </header>
+      <Header 
+        title="This is title"
+        descr="This is description"
+      />
+      <Layout 
+        title="This is title layout1"
+        descr="This is description layout1"
+        urlBg={background1}
+      />
+      <Layout 
+        title="This is title layout2"
+        descr="This is description layout2"
+        urlBg={background2}
+      />
+      <Layout 
+        title="This is title layout3"
+        descr="This is description layout3"
+      />
+      <Footer />
     </div>
   );
 }
