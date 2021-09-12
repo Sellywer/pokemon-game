46fde4fa954f7cfdee189471f1bf4dc7007d5d47
Адель <selly.lady.zakirova@gmail.com>
2021-09-12 12:22:36 +0500

    Revert "Lesson 2"
    This reverts commit 725ceebb4045a5b9973aac84a887f2a29a3b3e50.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header';
-import Layout from './components/layout/layout';
-import Footer from './components/footer/footer';
-import background1 from './assets/bg1.jpeg';
-import background2 from './assets/bg2.jpeg';
-
 
 function App() {
   return (
     <div className="App">
-      <Header 
-        title="This is title"
-        descr="This is description"
-      />
-      <Layout 
-        title="This is title layout1"
-        descr="This is description layout1"
-        urlBg={background1}
-      />
-      <Layout 
-        title="This is title layout2"
-        descr="This is description layout2"
-        urlBg={background2}
-      />
-      <Layout 
-        title="This is title layout3"
-        descr="This is description layout3"
-      />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world, React.JS!
+        </p>
+      </header>
     </div>
   );
 }
